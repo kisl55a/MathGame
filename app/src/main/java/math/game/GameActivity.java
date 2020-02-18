e5b5dd15d90db99174ea0f58775f5378ca5eb5c6
Dmitrii <kislenko1910@mail.ru>
2020-02-18 17:45:08 +0200

    Main idea is done

--- app/src/main/java/math/game/GameActivity.java
+++ app/src/main/java/math/game/GameActivity.java
@@ -3,29 +3,122 @@ package math.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class GameActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class GameActivity extends AppCompatActivity implements View.OnClickListener{
+    int correctAnswer;
+    Button buttonObjectChoice1;
+    Button buttonObjectChoice2;
+    Button buttonObjectChoice3;
+    TextView textObjectPartA;
+    TextView textObjectPartB;
+    TextView textObjectScore;
+    TextView textObjectLevel;
+    int currentScore = 0;
+    int currentLevel = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        int partA = 9;
-        int partB = 9;
-        int correctAnswer = partA * partB;
-        int wrongAnswer1 = correctAnswer - 1;
-        int wrongAnswer2 = correctAnswer + 1;
-        TextView textObjectPartA =(TextView)findViewById(R.id.textPartA);
-        TextView textObjectPartB =(TextView)findViewById(R.id.textPartB);
-        Button buttonObjectChoice1 =(Button)findViewById(R.id.buttonChoice3);
-        Button buttonObjectChoice2 =(Button)findViewById(R.id.buttonChoice2);
-        Button buttonObjectChoice3 =(Button)findViewById(R.id.buttonChoice1);
-        textObjectPartA.setText("" + partA);
-        textObjectPartB.setText("" + partB);
-        buttonObjectChoice1.setText("" + correctAnswer);
-        buttonObjectChoice2.setText("" + wrongAnswer1);
-        buttonObjectChoice3.setText("" + wrongAnswer2);
+//        int partA = 9;
+//        int partB = 9;
+//        correctAnswer = partA * partB;
+//        int wrongAnswer1 = correctAnswer - 1;
+//        int wrongAnswer2 = correctAnswer + 1;
+        textObjectPartA =(TextView)findViewById(R.id.textPartA);
+        textObjectPartB =(TextView)findViewById(R.id.textPartB);
+        buttonObjectChoice2 =(Button)findViewById(R.id.buttonChoice2);
+        buttonObjectChoice3 =(Button)findViewById(R.id.buttonChoice3);
+        buttonObjectChoice1 =(Button)findViewById(R.id.buttonChoice1);
+        textObjectScore = (TextView)findViewById(R.id.textScore);
+        textObjectLevel = (TextView)findViewById(R.id.textLevel);
+//        textObjectPartA.setText("" + partA);
+//        textObjectPartB.setText("" + partB);
+//        buttonObjectChoice1.setText("" + correctAnswer);
+//        buttonObjectChoice2.setText("" + wrongAnswer1);
+//        buttonObjectChoice3.setText("" + wrongAnswer2);
+        buttonObjectChoice1.setOnClickListener(this);
+        buttonObjectChoice2.setOnClickListener(this);
+        buttonObjectChoice3.setOnClickListener(this);
+        setQuestion();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int answerGiven=0;
+        switch (v.getId()) {
+            case R.id.buttonChoice1:
+                answerGiven = Integer.parseInt("" + buttonObjectChoice1.getText());
+                break;
+            case R.id.buttonChoice2:
+                answerGiven = Integer.parseInt("" + buttonObjectChoice2.getText());
+                break;
+            case R.id.buttonChoice3:
+                answerGiven = Integer.parseInt("" + buttonObjectChoice3.getText());
+                break;
+        }
+        updateScoreAndLevel(answerGiven);
+        setQuestion();
+    }
+    void setQuestion(){
+        int numberRange = currentLevel * 3;
+        Random randInt = new Random();
+        int partA = randInt.nextInt(numberRange);
+        partA++;
+        int partB = randInt.nextInt(numberRange);
+        partB++;
+        correctAnswer = partA * partB;
+        int wrongAnswer1 = correctAnswer-2;
+        int wrongAnswer2 = correctAnswer+2;
+        textObjectPartA.setText(""+partA);
+        textObjectPartB.setText(""+partB);
+        int buttonLayout = randInt.nextInt(3);
+        switch (buttonLayout){
+            case 0:
+                buttonObjectChoice1.setText(""+correctAnswer);
+                buttonObjectChoice2.setText(""+wrongAnswer1);
+                buttonObjectChoice3.setText(""+wrongAnswer2);
+                break;
+            case 1:
+                buttonObjectChoice2.setText(""+correctAnswer);
+                buttonObjectChoice3.setText(""+wrongAnswer1);
+                buttonObjectChoice1.setText(""+wrongAnswer2);
+                break;
+            case 2:
+                buttonObjectChoice3.setText(""+correctAnswer);
+                buttonObjectChoice1.setText(""+wrongAnswer1);
+                buttonObjectChoice2.setText(""+wrongAnswer2);
+                break;
+        }
+    }
+
+    void updateScoreAndLevel(int answerGiven){
+        if(isCorrect(answerGiven)){
+            for(int i = 1; i <= currentLevel; i++){
+                currentScore = currentScore + i;
+            }
+            currentLevel++;
+        } else {
+            currentScore = 0;
+            currentLevel = 1;
+        }
+        textObjectScore.setText("Score: " + currentScore);
+        textObjectLevel.setText("Level: " + currentLevel);
+    }
+    boolean isCorrect(int answerGiven){
+        boolean correctTrueOrFalse;
+        if(answerGiven == correctAnswer){
+            Toast.makeText(getApplicationContext(), "Well done!", Toast.LENGTH_LONG).show();
+            correctTrueOrFalse=true;
+        } else {
+            Toast.makeText(getApplicationContext(), "Sorry", Toast.LENGTH_LONG).show();
+            correctTrueOrFalse=false;
+        }
+        return correctTrueOrFalse;
     }
 }
